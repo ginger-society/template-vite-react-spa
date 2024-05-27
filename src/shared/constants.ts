bc8ff4c08c372a2d33e750cf83e6025d791420f1
Ginger Society Admin <admin@gingersociety.org>
2024-05-27 18:13:21 +0530

    feat: cleanup

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -10,12 +10,3 @@ export const ROUTES = {
   SIGN_IN: "/auth/sign-in",
   HOME: "/home",
 };
-
-export const DataUploadTitleEnum = {
-  "qp-data": "QP Data Load",
-  "utility-data": "Utility Data Load",
-  "all-etl-jobs": "ETL Job List",
-};
-
-export const BASE_URL =
-  "http://ecs-services-204813822.us-east-1.elb.amazonaws.com/";
