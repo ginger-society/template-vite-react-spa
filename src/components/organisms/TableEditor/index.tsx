1edb25c3b8d13ed2348d15a444322095ecd2a62e
Ginger Society Admin <admin@gingersociety.org>
2024-05-28 23:19:01 +0530

    chore : minor cleanup

--- src/components/organisms/TableEditor/index.tsx
+++ src/components/organisms/TableEditor/index.tsx
@@ -1,17 +1,25 @@
 import { useUMLEditor } from "../UMLEditor/context";
 
 const TableEditor = () => {
-  const { blocks, setBlocks, connections, setConnections } = useUMLEditor();
+  const {
+    blocks,
+    setBlocks,
+    connections,
+    setConnections,
+    editorData = { blockId: "" },
+  } = useUMLEditor();
 
   const handleNameChange = ({
     target: { value },
   }: React.ChangeEvent<HTMLInputElement>) => {
+    if (!editorData?.blockId) {
+      return;
+    }
     setBlocks((v) => {
-      console.log(v["no-id"]);
       return {
         ...v,
-        ["no-id"]: {
-          ...v["no-id"],
+        [editorData?.blockId]: {
+          ...v[editorData?.blockId],
           data: { name: value },
         },
       };
@@ -22,10 +30,10 @@ const TableEditor = () => {
     <>
       Table editor here
       <h2>Slider Window Content {Object.keys(blocks).length}</h2>
-      {JSON.stringify(blocks["no-id"].data)}
+      {JSON.stringify(blocks[editorData?.blockId].data)}
       <input
         style={{ border: "solid 1px" }}
-        value={blocks["no-id"].data.name}
+        value={blocks[editorData?.blockId].data.name}
         onChange={handleNameChange}
       />
     </>
