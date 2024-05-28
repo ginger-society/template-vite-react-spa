63daf3ab4c4e9b12f753412e50c569ff74c25cc4
Ginger Society Admin <admin@gingersociety.org>
2024-05-28 12:54:01 +0530

    feat: refactored icons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,12 @@ import { mockBlocks, mockConnections } from "@/shared/mocks";
 import { Block, Connection } from "@/shared/types";
 import React, { useState, useEffect, useCallback } from "react";
 import Draggable from "react-draggable";
+import {
+  hexagonIcon,
+  triangleIcon,
+  rectangleIcon,
+  pencilIcon,
+} from "@/shared/svgIcons";
 
 const Home = () => {
   const [isSliderOpen, setIsSliderOpen] = useState(false);
@@ -78,24 +84,7 @@ const Home = () => {
                   }}
                   className="handle"
                 >
-                  <svg
-                    className="editIcon"
-                    fill="#000000"
-                    version="1.1"
-                    id="Capa_1"
-                    width="800px"
-                    height="800px"
-                    viewBox="0 0 528.899 528.899"
-                  >
-                    <g>
-                      <path
-                        d="M328.883,89.125l107.59,107.589l-272.34,272.34L56.604,361.465L328.883,89.125z M518.113,63.177l-47.981-47.981
-		c-18.543-18.543-48.653-18.543-67.259,0l-45.961,45.961l107.59,107.59l53.611-53.611
-		C532.495,100.753,532.495,77.559,518.113,63.177z M0.3,512.69c-1.958,8.812,5.998,16.708,14.811,14.565l119.891-29.069
-		L27.473,390.597L0.3,512.69z"
-                      />
-                    </g>
-                  </svg>
+                  {pencilIcon}
                 </span>
               </div>
               {/* Render dynamic number of rows */}
@@ -117,26 +106,13 @@ const Home = () => {
                   {(() => {
                     switch (connections[index].marker) {
                       case "triangle":
-                        return <polygon points="-6,-6 6,0 -6,6" fill="red" />;
+                        return triangleIcon;
                       case "square":
-                        return (
-                          <rect
-                            x="-6"
-                            y="-6"
-                            width="12"
-                            height="12"
-                            fill="blue"
-                          />
-                        );
+                        return rectangleIcon;
                       case "circle":
                         return <circle cx="0" cy="0" r="6" fill="green" />;
                       case "hexagon":
-                        return (
-                          <polygon
-                            points="-6,-3 0,-6 6,-3 6,3 0,6 -6,3"
-                            fill="orange"
-                          />
-                        );
+                        return hexagonIcon;
                       default:
                         return null;
                     }
